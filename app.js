cf34d34c1e3b9032c8f95dcea091bbe6e54fc313
maivlys <122049862+maivlys@users.noreply.github.com>
2025-01-16 20:11:07 +0100

    implementation logic also displayed in ui + strucuture of the code

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const cells = Array.from(document.getElementsByClassName("cell"));
-console.log(cells);
-
 // -----Player
 const Player = (() => {
   let currentPlayer = "X";
+
   const getPlayer = () => currentPlayer;
   const switchPlayer = () => {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
   };
+
   return { getPlayer, switchPlayer };
 })();
 
@@ -16,6 +15,11 @@ const Gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
   const getBoard = () => board;
   const resetBoard = () => (board = ["", "", "", "", "", "", "", "", ""]);
+
+  let boardDisabled = false;
+  const isDisabled = () => boardDisabled;
+  const changeDisability = () =>
+    (boardDisabled = boardDisabled === true ? false : true);
 
   const checkWin = () => {
     const winningCombinaitons = [
@@ -28,40 +32,92 @@ const Gameboard = (() => {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     for (const combo of winningCombinaitons) {
       const [a, b, c] = combo;
       let board = getBoard();
-      console.log(board);
       if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]) {
-        console.log(`Winner: Player ${Player.getPlayer()}`);
+        changeDisability();
+        UI.winningBoard(combo);
+        return true;
       }
     }
   };
 
   const makeMove = (el) => {
-    // const cell = el.target;
-    const i = el.target.id;
-    if (board[i] === "") {
-      board[i] = Player.getPlayer();
-      checkWin();
-      Player.switchPlayer();
+    const i = el.id;
+    if (board[i] !== "") {
       return;
     }
-    console.log("spot taken");
+    board[i] = Player.getPlayer();
+    UI.displayMove(i);
+    if (!checkWin()) {
+      Player.switchPlayer();
+      UI.displayStatus().textContent = `${Player.getPlayer()}'s turn`;
+    }
   };
-  return { getBoard, resetBoard, makeMove };
+
+  const playAgain = () => {
+    Gameboard.resetBoard();
+    Gameboard.changeDisability();
+    UI.init();
+  };
+
+  return {
+    isDisabled,
+    changeDisability,
+    getBoard,
+    resetBoard,
+    makeMove,
+    playAgain,
+  };
 })();
 
-const playAgain = () => {
-  Gameboard.resetBoard();
-};
-
 // -----Display
-cells.forEach((el) => {
-  el.addEventListener("click", Gameboard.makeMove);
-});
+const UI = (() => {
+  const gameboard = document.getElementById("gameBoard");
+  const cells = Array.from(document.getElementsByClassName("cell"));
 
-const resetBtn = document.getElementById("reset-btn");
-resetBtn.addEventListener("click", playAgain);
+  const status = document.getElementById("status");
+  const displayStatus = () => status;
 
-let status = document.getElementById("status");
+  const resetBtn = document.getElementById("reset-btn");
+  resetBtn.addEventListener("click", Gameboard.playAgain);
+
+  const init = () => {
+    UI.displayStatus().textContent = `${Player.getPlayer()}'s turn`;
+    cells.forEach((cell) => {
+      cell.classList.remove("winning-x");
+      cell.classList.remove("winning-o");
+      cell.textContent = "";
+      gameboard.style.cursor = "pointer";
+      cell.addEventListener("click", () => {
+        if (!Gameboard.isDisabled()) {
+          Gameboard.makeMove(cell);
+          return;
+        }
+      });
+    });
+  };
+
+  const winningBoard = (combo) => {
+    gameboard.style.cursor = "default";
+    combo.forEach((id) => {
+      if (Player.getPlayer() === "X") {
+        document.getElementById(id).classList.add("winning-x");
+      } else {
+        document.getElementById(id).classList.add("winning-o");
+      }
+    });
+
+    UI.displayStatus().textContent = `WINNER IS: ${Player.getPlayer()} !`;
+  };
+
+  const displayMove = (i) => {
+    document.getElementById([i]).textContent = Player.getPlayer();
+  };
+
+  return { init, displayMove, displayStatus, winningBoard };
+})();
+
+UI.init();
